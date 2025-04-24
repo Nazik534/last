1c3c625d39f3d9352287fabaf1c86230616bc0e3
Nazik534 <157049844+Nazik534@users.noreply.github.com>
2025-04-24 17:39:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+// Знаходимо елемент за класом
+let them = document.querySelector('.them');
+
+them.addEventListener("click", mouserr );
+
+let body = document.querySelector('body');
+
+let header = document.querySelector('header');
+
+let title = document.querySelector('.title');
+
+let ab = document.querySelector('.ab');
+
+let ab1 = document.querySelector('.ab1');
+
+let ab2 = document.querySelector('.ab2');
+
+let footer = document.querySelector('footer')
+
+let klass = document.querySelectorAll('.klass')
+
+
+let them2 = "white"
+
+
+function  mouserr(){
+   if (them2 == "white"){
+   body.style.backgroundColor = '#000000';
+   header.style.backgroundColor = '#ffffff'
+   title.style.color = '#ffffff'
+   ab.style.color = '#ffffff'
+   ab1.style.color = '#ffffff'
+   ab2.style.color = '#ffffff'
+   footer.style.backgroundColor = '#ffffff'
+   for (let i=0;i<h3.length;i+=1){
+      h3[i].style.background = "#ffffff"
+      h3[i].style.color  = "#000000"
+      console.log("привіт")
+   }
+   for (let i=0;i<klass.length;i+=1){
+       klass[i].style.color  = "#000000"
+       console.log("привіт")
+   }
+   them2 = "black"
+}
+   else {
+      body.style.backgroundColor = '#ffffff';
+      header.style.backgroundColor = '#000000'
+      title.style.color = '#000000'
+      ab.style.color = '#000000'
+      ab1.style.color = '#000000'
+      ab2.style.color = '#000000'
+      footer.style.backgroundColor = '#000000'
+      for (let i=0;i<h3.length;i+=1){
+         h3[i].style.background = "#000000"
+         h3[i].style.color  = "#ffffff"
+         console.log("привіт")
+      }
+      for (let i=0;i<klass.length;i+=1){
+          klass[i].style.color  = "#ffffff"
+          console.log("привіт")
+      }
+   them2 = "white"
+   }
+}
+
